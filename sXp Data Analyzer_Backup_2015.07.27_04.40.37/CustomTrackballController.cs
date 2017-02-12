028e47022aa3d2493c6260e2821034fb3623f047
Tayler How <taylerhow@gmail.com>
2017-02-11 23:58:29 -0500

    Added sample sXp input to resources

---
+++ sXp Data Analyzer_Backup_2015.07.27_04.40.37/CustomTrackballController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Charting;
+using Telerik.WinControls.UI;
+
+namespace sXp_Data_Analyzer
+{
+    public class CustomTrackballController : ChartTrackballController
+    {
+        protected override string GetTrackballText(List<DataPointInfo> points)
+        {
+            StringBuilder result = new StringBuilder("<html>");
+
+            SortedDictionary<ChartSeries, List<DataPoint>> visiblePoints = new SortedDictionary<ChartSeries, List<DataPoint>>(new ChartSeriesComparer());
+
+            foreach (DataPointInfo pointInfo in points)
+            {
+                if (visiblePoints.ContainsKey(pointInfo.Series))
+                {
+                    visiblePoints[pointInfo.Series].Add(pointInfo.DataPoint);
+                }
+                else
+                {
+                    visiblePoints.Add(pointInfo.Series, new List<DataPoint>() { pointInfo.DataPoint });
+                }
+            }
+
+            int counter = 0;
+            foreach (ChartSeries series in visiblePoints.Keys)
+            {
+                for (int i = 0; i < visiblePoints[series].Count; i++)
+                {
+                    Color pointColor = this.GetColorForDataPoint(series, visiblePoints[series][i]);
+                    string color = string.Format("{0},{1},{2},{3}", pointColor.A, pointColor.R, pointColor.G, pointColor.B);
+                    result.AppendFormat("<color={0}>{1}", color, this.GetPointText(visiblePoints[series][i]));
+
+                    if (i < visiblePoints[series].Count)
+                    {
+                        result.Append(" ");
+                    }
+                }
+
+                counter++;
+
+                if (counter < visiblePoints.Keys.Count)
+                {
+                    result.Append("\n");
+                }
+            }
+
+            result.Append("</html>");
+
+            return result.ToString();
+        }
+
+        class ChartSeriesComparer : IComparer<ChartSeries>
+        {
+            public ChartSeriesComparer()
+            {
+            }
+
+            public int Compare(ChartSeries x, ChartSeries y)
+            {
+                if (!(x is IndicatorBase) && y is IndicatorBase)
+                {
+                    return -1;
+                }
+                else if (x is IndicatorBase && !(y is IndicatorBase))
+                {
+                    return 1;
+                }
+
+                return x.GetHashCode().CompareTo(y.GetHashCode());
+            }
+        }
+    }
+}
---
+++ sXp Data Analyzer_Backup_2015.07.27_04.40.37/sXpChannel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.WinControls.UI;
+
+namespace sXp_Data_Analyzer
+{
+    class sXpChannel
+    {
+        public sXpChannel(string name)
+        {
+            this.Name = name;
+            this.DisplayMag = false;
+            this.DisplayPhase = false;
+        }
+
+        public string Name { get; set; }
+
+        public bool DisplayMag { get; set; }
+
+        public bool DisplayPhase { get; set; }
+
+        public LineSeries MagSeries { get; set; }
+
+        public LineSeries PhaseSeries { get; set; }
+    }
+}
